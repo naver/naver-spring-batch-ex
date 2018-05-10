7da3429cff75e84c8830777a9422d2dc7a2b781d
fomuon <fomuon@gmail.com>
2018-05-11 02:21:21 +0900

    유닛테스트 설정

---
+++ naver-spring-batch-example/src/test/java/com/naver/spring/batch/sample/job/AbstractJobTest.java
@@ -0,0 +1,32 @@
+package com.naver.spring.batch.sample.job;
+
+import com.naver.spring.batch.sample.config.BatchConfig;
+import org.junit.runner.RunWith;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureJdbc;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author fomuo@navercorp.com
+ */
+@RunWith(SpringRunner.class)
+@AutoConfigureJdbc
+@Import({ BatchConfig.class, AbstractJobTest.TestConfig.class })
+@TestPropertySource(properties = "spring.batch.job.enabled=false")
+public abstract class AbstractJobTest {
+	@Autowired
+	protected JobLauncherTestUtils jobLauncherTestUtils;
+
+	@TestConfiguration
+	static class TestConfig {
+		@Bean
+		public JobLauncherTestUtils jobLauncherTestUtils() {
+			return new JobLauncherTestUtils();
+		}
+	}
+}
--- naver-spring-batch-example/src/test/java/com/naver/spring/batch/sample/job/sample/ImportUserJobConfigTest.java
+++ naver-spring-batch-example/src/test/java/com/naver/spring/batch/sample/job/sample/ImportUserJobConfigTest.java
@@ -1,25 +1,17 @@
 package com.naver.spring.batch.sample.job.sample;
 
+import com.naver.spring.batch.sample.job.AbstractJobTest;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author fomuo@navercorp.com
  */
-@RunWith(SpringRunner.class)
-@Import({TestConfig.class, ImportUserJobConfig.class })
-@SpringBootTest
-public class ImportUserJobConfigTest {
-	@Autowired
-	private JobLauncherTestUtils jobLauncherTestUtils;
+@SpringBootTest(classes = ImportUserJobConfig.class)
+public class ImportUserJobConfigTest extends AbstractJobTest {
 
 	@Test
 	public void importUserJob() throws Exception {
