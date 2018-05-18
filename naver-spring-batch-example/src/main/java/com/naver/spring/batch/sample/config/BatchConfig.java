c48294c1bf98342e5a50bc0353d8ce876667c861
이용규 <yongkyu.lee@navercorp.com>
2018-05-19 01:31:43 +0900

    (sample) batch JdbcRepository 로 사용하도록 수정

--- naver-spring-batch-example/src/main/java/com/naver/spring/batch/sample/config/BatchConfig.java
+++ naver-spring-batch-example/src/main/java/com/naver/spring/batch/sample/config/BatchConfig.java
@@ -5,15 +5,13 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-import javax.sql.DataSource;
-
-@ImportResource(value = "classpath:com/naver/spring/batch/sample/job/sample3_validation/BeanValidationJobConfig.xml")
+@ImportResource(value = "classpath:jobs/BeanValidationJobConfig.xml")
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig extends DefaultBatchConfigurer {
 
 	//배치 처리정보를 DB에 처리하지 않고 메모리에서 처리하도록
-	@Override
-	public void setDataSource(DataSource dataSource) {
-	}
+//	@Override
+//	public void setDataSource(DataSource dataSource) {
+//	}
 }
