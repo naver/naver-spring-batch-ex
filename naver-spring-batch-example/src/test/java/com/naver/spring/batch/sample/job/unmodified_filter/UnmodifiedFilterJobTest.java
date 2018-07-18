ac86ed86a4d808c8304c6b5e8e0490fea74aaff6
이용규 <yongkyu.lee@navercorp.com>
2018-07-18 17:46:40 +0900

    unmodified item filter skip 기능 테스트 추가

--- naver-spring-batch-example/src/test/java/com/naver/spring/batch/sample/job/unmodified_filter/UnmodifiedFilterJobTest.java
+++ naver-spring-batch-example/src/test/java/com/naver/spring/batch/sample/job/unmodified_filter/UnmodifiedFilterJobTest.java
@@ -1,18 +1,30 @@
 package com.naver.spring.batch.sample.job.unmodified_filter;
 
 import com.naver.spring.batch.sample.job.AbstractJobTest;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = UnmodifiedFilterJobConfig.class)
 //@TestPropertySource(properties = "logging.level.com.naver.spring.batch.sample.job.unmodified_filter=warn")
 public class UnmodifiedFilterJobTest extends AbstractJobTest {
 	@Test
-	public void unmodifiedFilterJob() throws Exception {
+	public void unmodifiedFilter_test() throws Exception {
 		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+		Assert.assertEquals(jobExecution.getStatus(), BatchStatus.COMPLETED);
+	}
+
+	@Test
+	public void unmodifiedFilterJob_skip_test() throws Exception {
+		JobParametersBuilder jpBuilder = new JobParametersBuilder();
+		jpBuilder.addString("UnmodifiedItemFilter-skip", "true");
+		jpBuilder.addLong("random", RandomUtils.nextLong());
+
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob(jpBuilder.toJobParameters());
 		Assert.assertEquals(jobExecution.getStatus(), BatchStatus.COMPLETED);
 	}
 }
